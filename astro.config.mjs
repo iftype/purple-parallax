8a8f577067bd1790a67826c7b94476e9a35eebc2
iftype <iftype@naver.com>
2025-11-05 00:49:17 +0900

    delete

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://iftype.github.io",
-  base: "/purple-parallax",
+  //   base: "/purple-parallax",
   integrations: [mdx(), sitemap()],
 });
